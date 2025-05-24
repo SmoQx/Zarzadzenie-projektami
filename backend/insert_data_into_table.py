735c0895a4fb11d4e7d2b989f6f19c9feb3323fe
smoqx <smoqxpl@gmail.com>
2025-05-24 17:29:55 +0200

    stworzono skrypt do wprowadzania zdjec do bazy

--- backend/insert_data_into_table.py
+++ backend/insert_data_into_table.py
@@ -1,8 +1,20 @@
 from db import db_execute
+from pathlib import Path
+from os import listdir
 
 
 def main():
-    pass
+    photos_dir = Path("./photos")
+    all_files_in_photo_dir = listdir(photos_dir)
+    for directory in all_files_in_photo_dir:
+        directory = photos_dir / Path(directory)
+        for file in listdir(directory):
+            if Path(file).suffix in [".jpg", ".png", ".jpeg"]:
+                file_name = Path(file).stem
+                print(file_name, "in directory", directory.name)
+                with open(f"file", "rb") as photo:
+                    binary_data = photo.read()
+                    db_execute.insert_data(table_name=directory.name, name=file_name, spaces=20, photo=binary_data)
 
 
 if __name__ == "__main__":
